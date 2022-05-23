41d02d9471532d07cdef0e158ec3a3e4a67f1328
Tony-Paul8 <51736425+Tony-Paul8@users.noreply.github.com>
2022-05-23 18:41:29 +0200

    Update scoretable.py
    scores = [[0]*3]*5 只是复制了第一个列表的地址，导致最终结果中 5个人的结果完全相同。
    按照以上修改后，可以确保每个人的成绩保存的结果不互相覆盖。

--- Day01-15/code/Day07/scoretable.py
+++ Day01-15/code/Day07/scoretable.py
@@ -10,12 +10,14 @@ Date: 2018-03-06
 def main():
     names = ['关羽', '张飞', '赵云', '马超', '黄忠']
     subjs = ['语文', '数学', '英语']
-    scores = [[0] * 3] * 5
+    scores = [[]] * 5
     for row, name in enumerate(names):
         print('请输入%s的成绩' % name)
+        scores[row] = [0] * 3
         for col, subj in enumerate(subjs):
             scores[row][col] = float(input(subj + ': '))
-    print(scores)
+        print(scores)
+        
 #   for row, name in enumerate(names):
 #       print('请输入%s的成绩' % name)
 #       scores[row] = [None] * len(subjs)
